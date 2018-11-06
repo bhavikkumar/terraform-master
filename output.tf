7fb8ca40caeb51b60e2607fe1d172c4c8779cad9
Bhavik Kumar <bhavikk85@gmail.com>
2018-11-06 23:37:13 +1300

    Hide sensitive outputs

--- output.tf
+++ output.tf
@@ -1,18 +1,22 @@
 output "master_account_alias" {
   value = "${aws_iam_account_alias.master.account_alias}"
+  sensitive = true
 }
 
 output "operations_account_id" {
   value = "${aws_organizations_account.operations.id}"
+  sensitive = true
 }
 
 output "operations_account_alias" {
   value = "${aws_iam_account_alias.operations.account_alias}"
+  sensitive = true
 }
 
 output "cloudtrail_bucket_id" {
   value       = "${module.cloudtrail.s3_bucket}"
   description = "CloudTrail bucket ID"
+  sensitive = true
 }
 
 output "terraform_bucket_id" {
@@ -23,6 +27,7 @@ output "terraform_bucket_id" {
 output "terraform_kms_key_arn" {
   value = "${module.terraform.kms_key_arn}"
   description = "Terraform KMS Key ARN"
+  sensitive = true
 }
 
 output "terraform_dynamodb_table_name" {
