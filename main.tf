d0641651a85f4bc948375cc372cf2043bd9554f7
Bhavik Kumar <bhavikk85@gmail.com>
2018-11-11 17:03:44 +1300

    Fix SCP permission to allow updates from Terraform and Admins

--- main.tf
+++ main.tf
@@ -290,11 +290,26 @@ resource "aws_organizations_policy" "scp-policy" {
       "Effect": "Deny",
       "Action": [
         "cloudtrail:StopLogging",
-        "cloudtrail:UpdateTrail",
         "cloudtrail:DeleteTrail",
         "cloudtrail:PutEventSelectors"
       ],
       "Resource": "*"
+    },
+    {
+      "Effect": "Deny",
+      "Action": [
+        "cloudtrail:UpdateTrail"
+      ],
+      "Resource": "*",
+      "NotPrincipal": {
+        "AWS": [
+              "arn:aws:sts::336840772780:assumed-role/OrganizationAccountAccessRole/terraform",
+              "arn:aws:iam::336840772780:root",
+              "arn:aws:iam::336840772780:role/Admin",
+              "arn:aws:iam::336840772780:role/OrganizationAccountAccessRole",
+              "arn:aws:sts::336840772780:assumed-role/Admin/terraform"
+        ]
+      }
     }
   ]
 }
